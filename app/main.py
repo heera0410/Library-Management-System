1cbf192b99374089601d3d423921ee8631d9d62b
heera0410 <heera.rajasekar0410@gmail.com>
2020-12-02 05:32:01 +0530

    Minor fix

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from crud import router as BookRouter
 
 app = FastAPI()
 
-app.include_router(BookRouter, tags=["Library"], prefix="/library")
-
-
 @app.get("/",tags=["Root"])
 async def read_root():
     return RedirectResponse("/docs/")
+
+app.include_router(BookRouter, tags=["Library"], prefix="/library")
+
+
